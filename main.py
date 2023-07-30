4e2bccf259bf85602eadbcf8ac1972333e3bb6bc
rgsk <guptarahul70322@gmail.com>
2023-07-30 16:40:29 +0530

    errors and ide setup

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pickle
 
 from fastapi import FastAPI
+from pandas import DataFrame
 
-popular_df = pickle.load(open('./model_exports/popular.pkl', 'rb'))
+popular_df: DataFrame = pickle.load(open('./model_exports/popular.pkl', 'rb'))
 
 
 app = FastAPI()
@@ -16,5 +17,12 @@ def hello():
 @app.get("/popular-books")
 def popular_books():
     # Convert DataFrame to list of dictionaries
-    records = popular_df.to_dict(orient='records')
+    renamed_popular_df = popular_df.rename(columns={
+        "Book-Title": 'book_name',
+        "Book-Author": 'author',
+        'Image-URL-M': "image",
+        'num_ratings': 'votes',
+        'avg_rating': 'rating'
+    })
+    records = renamed_popular_df.to_dict(orient='records')
     return records
---
+++ practice.py
@@ -0,0 +1,9 @@
+#  mypy practice.py && python practice.py
+
+names: dict[str, dict[str, int]] = {
+    'Rahul': {
+        'age': 22,
+        'hii': 1
+    }
+}
+print(names)
